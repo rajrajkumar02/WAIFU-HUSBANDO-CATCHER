8a5f81b4b2c5a2b3d2ab0d0fcd5e104992e4e2b7
rajrajkumar02 <sayanray089@gmail.com>
2025-04-19 18:44:14 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    OWNER_ID = "7809385991"
+    sudo_users = "7639271205", "8191247703"
+    GROUP_ID = --1002566763639
+    TOKEN = "7621110601:AAEDfn2i3mfg9wP-v0dzDohKD_hKb2x4Fak"
+    mongo_url = "mongodb+srv://sayanray089:sayanray089@cluster0.zbvi7fx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    PHOTO_URL = ["https://envs.sh/Yjd.jpg", "https://envs.sh/Yjd.jpg"]
+    SUPPORT_CHAT = "bot_databese"
+    UPDATE_CHAT = "bot_databese"
+    BOT_USERNAME = "Raiden_waifu_bot"
+    CHARA_CHANNEL_ID = "-1002566763639"
+    api_id = 20160788
+    api_hash = "192838ffc9ddeaf78ef96cb48aec7aa5"
 
     
 class Production(Config):
